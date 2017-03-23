291033820f4602c50f31f874698073b7b0f2c69e
Ritiek Malhotra <Ritiek@users.noreply.github.com>
2017-03-23 01:38:21 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info > (3,0):
 from setuptools import setup, find_packages
 
 setup(name='AskQuora',
-      version='0.1.0',
+      version='0.1.2',
       description='Quora Q&A right from the command line!',
       author='Ritiek Malhotra',
       author_email='ritiekmalhotra123@gmail.com',
@@ -24,6 +24,7 @@ setup(name='AskQuora',
       url='https://www.github.com/Ritiek/AskQuora',
       keywords=['quora', 'terminal', 'command-line', 'question', 'python'],
       license='MIT',
+      download_url='https://github.com/Ritiek/AskQuora/archive/v0.1.2.tar.gz',
       classifiers=[],
       install_requires=[
             'requests',
