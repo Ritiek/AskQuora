61fcaa6272b7d871a7d2dcd2904da1001d2da408
Ritiek Malhotra <Ritiek@users.noreply.github.com>
2017-04-01 16:49:36 +0530

    bump v0.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info > (3,0):
 from setuptools import setup, find_packages
 
 setup(name='AskQuora',
-      version='0.2.0',
+      version='0.2.1',
       description='Quora Q&A right from the command line!',
       author='Ritiek Malhotra',
       author_email='ritiekmalhotra123@gmail.com',
@@ -23,7 +23,7 @@ setup(name='AskQuora',
       url='https://www.github.com/Ritiek/AskQuora',
       keywords=['quora', 'terminal', 'command-line', 'question', 'python'],
       license='MIT',
-      download_url='https://github.com/Ritiek/AskQuora/archive/v0.2.0.tar.gz',
+      download_url='https://github.com/Ritiek/AskQuora/archive/v0.2.1.tar.gz',
       classifiers=[],
       install_requires=[
             'requests',
