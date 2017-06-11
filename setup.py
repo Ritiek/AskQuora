c75e95ac3a8dc9b9f5158d493e54473a6ad646f5
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2017-06-12 01:17:03 +0530

    Change CLI function

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ from setuptools import setup, find_packages
 import askquora
 
 setup(name='AskQuora',
-      version='0.2.2',
-      description='Quora Q&A right from the command line!',
+      version='0.2.3',
+      description='Quora Q&A right from the command line',
       author='Ritiek Malhotra',
       author_email='ritiekmalhotra123@gmail.com',
       packages = find_packages(),
       entry_points={
             'console_scripts': [
-                  'askquora = askquora.askquora:cli',
+                  'askquora = askquora.askquora:askquora',
             ]
       },
       url='https://www.github.com/Ritiek/AskQuora',
       keywords=['quora', 'terminal', 'command-line', 'question', 'python'],
       license='MIT',
-      download_url='https://github.com/Ritiek/AskQuora/archive/v0.2.2.tar.gz',
+      download_url='https://github.com/Ritiek/AskQuora/archive/v0.2.3.tar.gz',
       classifiers=[],
       install_requires=[
             'requests',
@@ -26,4 +26,3 @@ setup(name='AskQuora',
             'requests-cache'
       ]
      )
-
