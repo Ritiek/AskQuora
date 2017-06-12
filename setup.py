752e44650d4042688b29dbe82819c8f257f04db9
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2017-06-12 14:31:53 +0530

    Bump v0.2.4

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ from setuptools import setup, find_packages
 import askquora
 
 setup(name='AskQuora',
-      version='0.2.3',
+      version='0.2.4',
       description='Quora Q&A right from the command line',
       author='Ritiek Malhotra',
       author_email='ritiekmalhotra123@gmail.com',
       packages = find_packages(),
       entry_points={
             'console_scripts': [
-                  'askquora = askquora.askquora:askquora',
+                  'askquora = askquora.askquora:command_line',
             ]
       },
       url='https://www.github.com/Ritiek/AskQuora',
       keywords=['quora', 'terminal', 'command-line', 'question', 'python'],
       license='MIT',
-      download_url='https://github.com/Ritiek/AskQuora/archive/v0.2.3.tar.gz',
+      download_url='https://github.com/Ritiek/AskQuora/archive/v0.2.4.tar.gz',
       classifiers=[],
       install_requires=[
             'requests',
